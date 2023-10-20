cb8a91359d9a5305a48223a70ef4bf859f3a4785
damian <damian@LAB3-MAESTRO>
2023-10-20 12:13:59 -0600

    Agregar archivos de proyecto.

---
+++ Segunda/Shared/Modelos/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Segunda.Shared.Modelos
+{
+    public class Producto
+    {
+        [Range(1,int.MaxValue,ErrorMessage ="Debe ser un entero positivo")]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="No debe ser vacio")]
+        public string? Nombre { get; set; }
+        [Required(ErrorMessage = "Debe seleccionar departamento")]
+        public string? Departamento { get; set; }
+    }
+}
